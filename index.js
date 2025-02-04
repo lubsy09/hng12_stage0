ea3ab316ad23f17c6e5b80588ed82b15375358a6
lubsy09 <lubabatuahmad60@gmail.com>
2025-02-04 18:05:25 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({
+    Email: "lubabatuahmad60@gmail.com",
+    current_datetime: new Date().toISOString(),
+    github_repo_url: "https://github.com/lubsy09/hng12_stage0.git"
+  });
+})
+
+app.listen( PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
